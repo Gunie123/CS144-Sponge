74ccd675cd9fcc102c3b13638a540cc158eb5446
Gunie123 <hysq123@qq.com>
2026-05-16 11:53:31 +0800

    Lab2 finished.

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -6,14 +6,30 @@
 // automated checks run by `make check_lab2`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    const auto &header = seg.header();
+    if (!_isn.has_value()) {
+        if (!header.syn)
+            return;
+        _isn = header.seqno;
+    }
+    uint64_t checkpoint = _reassembler.stream_out().bytes_written() + 1;
+    uint64_t abs_seqno = unwrap(header.seqno, _isn.value(), checkpoint);
+    uint64_t stream_index = abs_seqno - 1 + (header.syn ? 1 : 0);
+    _reassembler.push_substring(seg.payload().copy(), stream_index, header.fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (!_isn.has_value())
+        return {};
+    uint64_t ackno = _reassembler.stream_out().bytes_written() + 1;
+    if (_reassembler.stream_out().input_ended())
+        ++ackno;
+    return wrap(ackno, _isn.value());
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return _capacity - _reassembler.stream_out().buffer_size(); }
